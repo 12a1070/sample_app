874fad30db2dfd64d53e51c1929bdfd88e368d10
teraitomoya <12a1070.sit.shonan@gmail.com>
2024-11-25 23:19:13 +0900

    6.2 ユーザーの検証、バリデーションの仕組み、一意性の問題_終了

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -48,4 +48,17 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
 end
