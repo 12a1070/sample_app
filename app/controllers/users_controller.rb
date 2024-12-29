9645c9bf6c96d52d99326f260e66c0367062b79b
teraitomoya <12a1070.sit.shonan@gmail.com>
2024-12-29 12:43:14 +0900

    10.3すべてのユーザーを表示する_からスタート

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,12 +47,13 @@ class UsersController < ApplicationController
     # ログイン済みユーザーかどうか確認
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url, status: :see_other
       end
     end
 
-    # 正しいユーザーかどうか確認
+      # 正しいユーザーかどうか確認
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url, status: :see_other) unless @user == current_user
