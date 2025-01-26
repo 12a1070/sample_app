91361662f93dd752d314e062552bdacfe5e16ce6
teraitomoya <12a1070.sit.shonan@gmail.com>
2025-01-26 23:56:08 +0900

    production.rbにrenderアプリのアドレス追加

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,8 +4,7 @@
 # bundle install
 # bundle exec rails assets:precompile
 # bundle exec rails assets:clean
-# # DISABLE_DATABASE_ENVIRONMENT_CHECK=1
-# # bundle exec rails db:migrate:reset
+# # DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
 # bundle exec rails db:seed
 #!/usr/bin/env bash
 # exit on error
