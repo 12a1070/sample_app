65abb0a91277d28e22239c0b21e021e6393eb5dd
teraitomoya <12a1070.sit.shonan@gmail.com>
2025-02-27 21:39:46 +0900

    migrateのみ

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,8 +4,8 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-# bundle exec rails db:migrate
+bundle exec rails db:migrate
 # bundle exec rails db:seed
 
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
 bundle exec rake db:seed
